127e35dbf495809483df97fe563acbb7f090adc0
Sean Kelly <seankwebdev@gmail.com>
2022-06-22 22:25:15 +0100

    Updated server to correctly fetch database string from .env

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express")
-const MongoClient = require("mongodb").MongoClient
 const app = express()
 const PORT = 8000
+
+require('dotenv').config()
+const MongoClient = require("mongodb").MongoClient
+// const client = new MongoClient(uri)
 
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+
 
 let db,
     dbConnectionStr = process.env.DB_STRING,
@@ -16,8 +20,8 @@ MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
     .then(client => {
         console.log(`Successfully connected to ${dbName}`)
         db = client.db(dbName)
-        const infoCollection = db.collection("spoon")
-        app.post('/quotes', (req, res) => {
+        const infoCollection = db.collection("poke")
+        app.post('/stats', (req, res) => {
             infoCollection.insertOne(req.body)
               .then(result => {
                 console.log(result)
