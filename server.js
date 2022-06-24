ab6e5d53c3edc84ce61bb0b8ef8bb9d08b3f73ee
Sean Kelly <seankwebdev@gmail.com>
2022-06-25 00:49:47 +0100

    Fixed load order and app.post to correctly post form data to Mongo.DB

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express")
 const app = express()
+const MongoClient = require("mongodb").MongoClient
 const bodyParser = require('body-parser')
 const PORT = 8000
-
 require('dotenv').config()
-const MongoClient = require("mongodb").MongoClient
+
+
+let db,
+    uri = process.env.ATLAS_URI
+    dbName = "poke-team-builder"
+
+MongoClient.connect(uri, {useUnifiedTopology: true})
+    .then(client => {
+        console.log(`Successfully connected to the Pokémon Team Builder Database`)
+        db = client.db(dbName)    
+})
 
 app.set("view engine", "ejs")
 app.use(express.static("public"))
@@ -12,34 +22,22 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
-
-const uri = process.env.ATLAS_URI
-
-
-MongoClient.connect(uri, {useUnifiedTopology: true})
-    .then(client => {
-        console.log(`Successfully connected to the Pokémon Team Builder Database`)
-        const db = client.db("poke_team_builder")
-        const infoCollection = db.collection("poke")
-        app.post('/stats', (req, res) => {
-            infoCollection.insertOne(req.body)
-              .then(result => {
-                res.redirect('/')
-              })
-              .catch(error => console.error(error))
-          })
-    })
-    .catch(error => console.error(error))
-
-app.listen(process.env.PORT || PORT, () => {
-    console.log(`The server is now running on port ${PORT}`)
-})
-
 app.get("/",(req,res) => {
     res.sendFile(__dirname + "/index.html")
 })
 
+
 app.post('/stats', (req, res) => {
-    console.log("Update successfully sent to the database.")
-    console.log(req.body)
-  })
+    db.collection("poke").insertOne(req.body)
+    .then(result => {
+        console.log("Update successfully sent to the database.")
+        console.log(req.body)
+        console.log(result)
+        res.redirect('/')
+    })
+    .catch(error => console.error(error))
+})
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`The server is now running on port ${PORT}`)
+})
