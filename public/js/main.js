5d0b793cf5dcd9eb0d666d2845f407f3d5b3ba0a
Sean Kelly <seankwebdev@gmail.com>
2022-06-28 00:32:24 +0100

    Programmed legend text to adapt to list length

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,31 @@
-const levelDown = document.querySelector('#levelDown')
-const levelUp = document.querySelector('#levelUp')
-let levelVal = document.querySelector('#levelVal').innerText
+const quantityColour = () => li_length < 6 ? teamQuantity.style.color = 'green' : teamQuantity.style.color = 'red'
 
-levelDown.addEventListener('click', () => { levelVal == 0 ? levelVal == 0 : levelVal -= 1 })
-levelUp.addEventListener('click', () => { levelVal == 100 ? levelVal == 100 : levelVal += 1 })
+window.onload = function() {
+    updateQuantity();
+    quantityColour()
+}
+
+const li_length = document.querySelectorAll(".current_team li").length
+const updateQuantity = () => teamQuantity.innerText = `(${li_length} of 6)`
+const teamQuantity = document.querySelector('#teamQuantity')
+const deletePokemon = document.querySelector('#deletePokemon')
+const submitForm = document.querySelector('#submit').addEventListener('click', updateQuantity, quantityColour)
+
+
+deletePokemon.addEventListener('click', () => {
+    const pName = this.parentNode.childNodes[3].innerText
+    fetch('/poke', {
+        method: 'delete',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            'name': pName
+        })
+    })
+    .then(res => {
+        if (res.ok) return res.json()
+    })
+    .then(data => {
+        window.location.reload()
+    })
+})
+
